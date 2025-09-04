e939052d86dabbd556da273fb28fbc76b51d3ada
Ritesh Deshpande <161418018+RiteshDeshpande17@users.noreply.github.com>
2025-09-05 00:12:37 +0530

    Add files via upload

---
+++ operators.java
@@ -0,0 +1,97 @@
+public class operators {
+    public static void main(String args[]){
+        
+        // Unary Operators
+
+        //Increament
+
+        // int a = 10;
+        // int b = ++a;
+        // System.out.println(a);
+        // System.out.println(b);
+
+
+        // int a = 10;
+        // int b = a++;
+        // System.out.println(a);
+        // System.out.println(b);
+
+        // Decreament
+
+        // int a = 10;
+        // int b = --a;
+        // System.out.println(a);
+        // System.out.println(b);
+
+
+        // int a = 10;
+        // int b = a--;
+        // System.out.println(a);
+        // System.out.println(b);
+
+
+        // Relational Operators 
+
+        // int A = 10;
+        // int B = 9;
+
+        // System.out.println(A==B);
+        // System.out.println(A!=B);
+        // System.out.println(A>B);
+        // System.out.println(A<B);
+        // System.out.println(A>=B);
+        // System.out.println(A<=B);
+
+
+        // Logical Operators
+
+        // Logical AND
+
+        // System.out.println((10>5) && (8>4));
+        // System.out.println((10>5) && (8<4));
+        // System.out.println((10<5) && (8>4));
+        // System.out.println((10<5) && (8<4));
+
+
+        // Logical OR
+
+        // System.out.println((10>5) || (8>4));
+        // System.out.println((10>5) || (8<4));
+        // System.out.println((10<5) || (8>4));
+        // System.out.println((10<5) || (8<4));
+
+
+        // Logical NOT
+
+        // System.out.println(!(2>3));
+
+
+
+        // Assignment Operators
+
+        int A = 5;
+        A += 10;
+
+        int B=20;
+        B -= 10;
+        
+        int C=20;
+        C *= 10;
+
+        int D=20;
+        D /= 10;
+
+        int E=20;
+        E %= 10;
+
+        System.out.println(A);
+        System.out.println(B);
+        System.out.println(C);
+        System.out.println(D);
+        System.out.println(E);
+
+
+    }
+}
+
+
