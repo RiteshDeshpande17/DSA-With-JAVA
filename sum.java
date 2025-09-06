cf905e8966923554a117778a38b45c1c1fd59a5d
Ritesh Deshpande <161418018+RiteshDeshpande17@users.noreply.github.com>
2025-09-06 18:50:21 +0530

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class sum {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        // int a = sc.nextInt();
+        // int b = sc.nextInt();
+
+        // int sum = a + b;
+        // int pr = a*b;
+
+        // System.out.println("The sum is: "+sum);
+
+        // System.out.println("The product is:"+pr);
+
+        float rad = sc.nextFloat();
+        float area = 3.14f * rad * rad;
+        System.out.println(area);
+
+        
+
+    }
+}
