2a7c420f291c256c3d18f081c7006c15f109456b
Ritesh Deshpande <161418018+RiteshDeshpande17@users.noreply.github.com>
2025-09-07 11:11:47 +0530

    Add files via upload

---
+++ Variables.java
@@ -0,0 +1,43 @@
+public class Variables {
+    public static void main(String args[]){
+        // int a = 10;
+        // int b = 20;
+        // String name = "Tony Stark";
+        
+        // System.out.println(a);
+        // System.out.println(b);
+        // System.out.println(name);
+
+        // a = b;
+        // System.out.print(a);
+
+
+       /*  byte b = 8;
+        System.out.println(b);
+
+        char ch = 'a';
+        System.out.println(ch);
+
+        boolean var = false;
+        System.out.print(var);
+
+        float price = 10.5f;
+        System.out.print(price);
+
+        int number =25;
+        System.out.print(number);
+
+        short n = 240;
+        System.out.print(n);
+
+        */
+
+
+        int a = 10;
+        int b = 20;
+        int sum = a+b;
+        System.out.print(sum);
+        
+
+    }
+}
