14dc740ffeb546159780f739a9b8ca46b1f5c52c
Ritesh Deshpande <161418018+RiteshDeshpande17@users.noreply.github.com>
2025-09-08 19:29:58 +0530

    Add files via upload

---
+++ JavaBasics.java
@@ -0,0 +1,35 @@
+// boilerplate code
+
+public class JavaBasics {
+    public static void main(String args[]){
+        System.out.print("Hello World!");
+
+        System.out.print("Ritesh!");
+
+        System.out.println("Ritesh!");    //
+        System.out.println("Ramdas");      // line space
+        System.out.println("Deshpande!");   //
+
+
+        System.out.print("I!\n");  //
+        System.out.print("Love\n");  // line space
+        System.out.print("you!\n");  //
+
+        System.out.print("Hello World!\nHello World!");
+        System.out.print("Hello World!");
+        System.out.print("Hello World!");
+
+        System.out.print("Hello World!\nHello\n"); // Hello World!\nHello\n world
+
+        System.out.println("*****");
+        System.out.println("****");
+        System.out.println("***");
+        System.out.println("**");
+        System.out.println("*");
+
+        
+
+
+    }
+}
+
